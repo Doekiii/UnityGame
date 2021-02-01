80bb6a679cff3969b2780862f3982f8621275b9e
Jodok <jackinator@hotmail.de>
2021-02-02 00:26:27 +0100

    Push1
    new Script (PlayerMovement)

---
+++ UnityGit/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() => std::cout << "Hello";
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
